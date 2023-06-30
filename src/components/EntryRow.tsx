d57dec1b68bfdc431b0e6266df934e0f54ab2722
ocboogie <boogie@mikulec.com>
2023-06-30 11:19:45 -0700

    Fix typescript error

--- src/components/EntryRow.tsx
+++ src/components/EntryRow.tsx
@@ -176,6 +176,7 @@ export default function EntryRow({
             <div className="ml-12 border-l">
               <EntryList
                 entries={followUps || []}
+                emptyLabel=""
                 isLoading={!followUps}
                 expandable
                 showEntryDates
