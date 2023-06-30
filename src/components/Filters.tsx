bed65cbe217277dd8a365e1363386cfc70c99b51
ocboogie <boogie@mikulec.com>
2023-06-30 09:38:15 -0700

    Add missing dependency to useEffect

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -24,7 +24,7 @@ export default function Filters({ filters, setFilters }: Props) {
     if (!logbooks) {
       fetchLogbooks().then((logbooks) => setLogbooks(logbooks));
     }
-  }, []);
+  }, [logbooks]);
 
   function logbookFilterLabel() {
     if (filters.logbooks.length === 0) {
