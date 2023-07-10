b4a0ab6f1a03e87189a7144ae72fca1abab12120
ocboogie <boogie@mikulec.com>
2023-07-10 16:20:54 -0700

    Remove debug log

--- src/hooks/useEntries.ts
+++ src/hooks/useEntries.ts
@@ -89,7 +89,6 @@ export default function useEntries({
   }, [searchText, logbooks, tags, entries, isLoading]);
 
   useEffect(() => {
-    console.log("happened");
     refreshEntries();
   }, [refreshEntries]);
 
