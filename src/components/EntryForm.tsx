43f7fe400426c70b22b77c22f6fc211bf304e10b
ocboogie <boogie@mikulec.com>
2023-07-14 14:29:25 -0700

    Make entry form preview selectable

--- src/components/EntryForm.tsx
+++ src/components/EntryForm.tsx
@@ -219,6 +219,7 @@ export default function EntryForm({
             showDate
             expandable
             showFollowUps
+            selectable
             allowSpotlight
           />
         </div>
