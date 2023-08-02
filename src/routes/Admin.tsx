243f2b342299ccf5bfc2f280226b88d16740e373
ocboogie <boogie@mikulec.com>
2023-08-02 10:39:15 -0700

    Add small navbar to admin dashboard

--- src/routes/Admin.tsx
+++ src/routes/Admin.tsx
@@ -1,13 +1,14 @@
 import { useCallback, useRef } from "react";
+import { toast } from "react-toastify";
+import { Link, useParams } from "react-router-dom";
+import cn from "classnames";
 import Pane from "../components/Pane";
 import Spinner from "../components/Spinner";
 import useIsSmallScreen from "../hooks/useIsSmallScreen";
-import cn from "classnames";
 import LogbookForm from "../components/LogbookForm";
 import { useLogbookFormsStore } from "../logbookFormsStore";
-import { toast } from "react-toastify";
-import { Link, useParams } from "react-router-dom";
 import useLogbooks from "../hooks/useLogbooks";
+import elogLogo from "../assets/temp_elog_logo.png";
 
 const MIN_PANE_WIDTH = 384;
 
@@ -47,59 +48,68 @@ export default function Admin() {
   }, [refresh]);
 
   return (
-    <div className="flex max-h-screen">
-      <div
-        ref={bodyRef}
-        className={cn(
-          "w-1/2 flex flex-col mt-3 justify-stretch p-3 overflow-y-auto",
-          // Don't want to have border when loading
-          logbooks ? "divide-y" : "justify-center w-full",
-          !selectedLogbook && "flex-1 pr-3"
-        )}
-      >
-        <div className="text-xl mb-2 font-normal text-gray-500">Logbooks</div>
-        {logbooks ? (
-          logbooks.map((logbook) => (
-            <Link
-              key={logbook.id}
-              to={`/admin/${logbook.id}`}
-              tabIndex={0}
-              className={cn(
-                "p-2 hover:bg-gray-100 cursor-pointer uppercase",
-                selectedLogbook?.id === logbook.id &&
-                  "bg-blue-100 hover:!bg-blue-200"
-              )}
-            >
-              {logbook.name}
-              <span className="text-gray-500">
-                {logbooksEdited.includes(logbook.id) && "*"}
-              </span>
-            </Link>
-          ))
-        ) : (
-          <Spinner className="self-center" />
-        )}
-      </div>
-      <div
-        className="relative border-r cursor-col-resize select-none"
-        onMouseDown={startDrag}
-        ref={gutterRef}
-      >
-        <div className="absolute -left-3 w-6 h-full" />
-      </div>
-      {selectedLogbook && (
-        <div
-          className={cn("pb-3", !isSmallScreen && "flex-1 flex-shrink")}
-          style={{ minWidth: isSmallScreen ? "auto" : MIN_PANE_WIDTH }}
-        >
-          <Pane home="/admin">
-            <br className="w-full" />
-            {selectedLogbook && (
-              <LogbookForm logbook={selectedLogbook} onSave={onSave} />
-            )}
-          </Pane>
+    <div className="flex flex-col max-h-screen">
+      <div className="p-2 shadow z-10">
+        <div className="container m-auto">
+          <Link to="/" className="text-center mb-3 w-full md:mb-0 md:w-auto">
+            <img src={elogLogo} className="inline" alt="SLAC E-LOG logo" />
+          </Link>
         </div>
-      )}
+      </div>
+      <div className="flex-1 flex overflow-hidden">
+        <div
+          ref={bodyRef}
+          className={cn(
+            "w-1/2 flex flex-col justify-stretch p-3 overflow-y-auto",
+            // Don't want to have border when loading
+            logbooks ? "divide-y" : "justify-center w-full",
+            !selectedLogbook && "flex-1 pr-3"
+          )}
+        >
+          <div className="text-xl mb-2 font-normal text-gray-500">Logbooks</div>
+          {logbooks ? (
+            logbooks.map((logbook) => (
+              <Link
+                key={logbook.id}
+                to={`/admin/${logbook.id}`}
+                tabIndex={0}
+                className={cn(
+                  "p-2 hover:bg-gray-100 cursor-pointer uppercase",
+                  selectedLogbook?.id === logbook.id &&
+                    "bg-blue-100 hover:!bg-blue-200"
+                )}
+              >
+                {logbook.name}
+                <span className="text-gray-500">
+                  {logbooksEdited.includes(logbook.id) && "*"}
+                </span>
+              </Link>
+            ))
+          ) : (
+            <Spinner className="self-center" />
+          )}
+        </div>
+        <div
+          className="relative border-r cursor-col-resize select-none"
+          onMouseDown={startDrag}
+          ref={gutterRef}
+        >
+          <div className="absolute -left-3 w-6 h-full" />
+        </div>
+        {selectedLogbook && (
+          <div
+            className={cn("pb-3", !isSmallScreen && "flex-1 flex-shrink")}
+            style={{ minWidth: isSmallScreen ? "auto" : MIN_PANE_WIDTH }}
+          >
+            <Pane home="/admin">
+              <br className="w-full" />
+              {selectedLogbook && (
+                <LogbookForm logbook={selectedLogbook} onSave={onSave} />
+              )}
+            </Pane>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
