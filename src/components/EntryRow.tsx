2f325510a383dcf29ae0211f92887c8b824d1016
ocboogie <boogie@mikulec.com>
2023-09-01 16:03:06 -0700

    Use mini magnifying glass for spotlight icon

--- src/components/EntryRow.tsx
+++ src/components/EntryRow.tsx
@@ -448,16 +448,14 @@ const EntryRow = memo(
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-6 h-6"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="w-5 h-5"
                   >
                     <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+                      fillRule="evenodd"
+                      d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+                      clipRule="evenodd"
                     />
                   </svg>
                 </RowButton>
