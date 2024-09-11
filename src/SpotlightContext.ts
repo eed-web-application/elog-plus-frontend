6d1bfc0a233c639cd964aab8f545d9d1eb15d447
ocboogie <boogie@mikulec.com>
2024-09-11 12:57:39 -0700

    Add comments

--- src/SpotlightContext.ts
+++ src/SpotlightContext.ts
@@ -1,6 +1,9 @@
 import { createContext } from "react";
 import { Entry } from "./api";
 
+/**
+ * Set the current spotlighted entry.
+ */
 const SpotlightContext = createContext<((entry: Entry) => void) | null>(null);
 
 export default SpotlightContext;
--- src/StickyEntryRowContext.ts
+++ src/StickyEntryRowContext.ts
@@ -5,6 +5,11 @@ export type Context = {
   usedHeight: number;
 };
 
+/**
+ * Since we have multiple sticky headers, we need to keep track of the total
+ * used height of all headers and the current z-index. This prevents the headers
+ * from overlapping each other and ensures that the headers currently stack.
+ */
 const StickyEntryRow = createContext<Context>({ zIndex: 100, usedHeight: 0 });
 
 export default StickyEntryRow;
