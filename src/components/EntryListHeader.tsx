70953fe4f989789f3e61b9d0ec492f6e09836ade
ocboogie <boogie@mikulec.com>
2024-09-12 13:41:13 -0700

    Move shift summary buttons to the right on desktop

--- src/components/EntryListHeader.tsx
+++ src/components/EntryListHeader.tsx
@@ -64,6 +64,105 @@ function getHeaderInfo(
   return info;
 }
 
+function ShiftButton({
+  shift: { id, name, logbookId, logbook },
+  summaryId,
+  date,
+}: {
+  shift: ShiftInfo;
+  summaryId?: string;
+  date: string;
+}) {
+  let label = name;
+
+  if (logbook) {
+    label = `${logbook?.toUpperCase()}:${label}`;
+  }
+
+  if (summaryId) {
+    return (
+      <Tooltip label="View shift summary">
+        <Link
+          to={{
+            pathname: `/${summaryId}`,
+            search: window.location.search,
+            hash: window.location.hash,
+          }}
+        >
+          <Chip
+            clickable
+            leftIcon={
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="mr-0.5 ml-1.5 w-4 h-4"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                />
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                />
+              </svg>
+            }
+          >
+            {label}
+          </Chip>
+        </Link>
+      </Tooltip>
+    );
+  } else {
+    return (
+      <Tooltip label="Summarize shift">
+        <Link
+          to={{
+            pathname: "/new-entry",
+            search: window.location.search,
+            hash: window.location.hash,
+          }}
+          state={{
+            logbooks: [logbookId],
+            summarizes: {
+              checked: true,
+              shiftId: id,
+              date: date,
+            },
+          }}
+        >
+          <Chip
+            clickable
+            leftIcon={
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="mr-0.5 ml-1.5 w-4 h-4"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                />
+              </svg>
+            }
+          >
+            {label}
+          </Chip>
+        </Link>
+      </Tooltip>
+    );
+  }
+}
+
 function getHeaderKey(
   logbooksIncluded: string[],
   entry: Entry,
@@ -83,8 +182,6 @@ const EntryListHeader = forwardRef<HTMLDivElement, Props>(
         ? representative.loggedAt
         : representative.eventAt,
     );
-
-    let summaryButton;
 
     let shiftIds: ShiftSummaryIdent[] = [];
 
@@ -107,109 +204,23 @@ const EntryListHeader = forwardRef<HTMLDivElement, Props>(
     return (
       <div
         className={twMerge(
-          "border-b gap-3 px-3 pt-1.5 pb-1 bg-gray-100 whitespace-nowrap",
+          "border-b px-3 bg-gray-100 whitespace-nowrap flex flex-wrap justify-between items-center md:gap-3 md:flex-nowrap content-center h-[60px] md:h-[40px]",
           className,
         )}
         ref={ref}
         {...rest}
       >
-        <div className="flex justify-between items-center">
-          <h3 className="text-lg truncate">{dateText}</h3>
-          {summaryButton}
-        </div>
-        <div className="flex gap-2">
+        <h3 className="text-lg truncate">{dateText}</h3>
+        <div className="flex gap-2 w-full md:w-auto">
           {!areSummariesLoading &&
-            shifts.map(({ id, name, logbook, logbookId }, index) => {
-              let label = name;
-
-              if (logbook) {
-                label = `${logbook?.toUpperCase()}:${label}`;
-              }
-
-              const summaryId = summaries[index];
-              if (summaryId) {
-                return (
-                  <Tooltip key={index} label="View shift summary">
-                    <Link
-                      to={{
-                        pathname: `/${summaryId}`,
-                        search: window.location.search,
-                        hash: window.location.hash,
-                      }}
-                    >
-                      <Chip
-                        clickable
-                        leftIcon={
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth={1.5}
-                            stroke="currentColor"
-                            className="mr-0.5 ml-1.5 w-4 h-4"
-                          >
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
-                            />
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                            />
-                          </svg>
-                        }
-                      >
-                        {label}
-                      </Chip>
-                    </Link>
-                  </Tooltip>
-                );
-              } else {
-                return (
-                  <Tooltip key={index} label="Summarize shift">
-                    <Link
-                      to={{
-                        pathname: "/new-entry",
-                        search: window.location.search,
-                        hash: window.location.hash,
-                      }}
-                      state={{
-                        logbooks: [logbookId],
-                        summarizes: {
-                          checked: true,
-                          shiftId: id,
-                          date: date,
-                        },
-                      }}
-                    >
-                      <Chip
-                        clickable
-                        leftIcon={
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth={1.5}
-                            stroke="currentColor"
-                            className="mr-0.5 ml-1.5 w-4 h-4"
-                          >
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              d="M7.5 14.25v2.25m3-4.5v4.5m3-6.75v6.75m3-9v9M6 20.25h12A2.25 2.25 0 0020.25 18V6A2.25 2.25 0 0018 3.75H6A2.25 2.25 0 003.75 6v12A2.25 2.25 0 006 20.25z"
-                            />
-                          </svg>
-                        }
-                      >
-                        {label}
-                      </Chip>
-                    </Link>
-                  </Tooltip>
-                );
-              }
-            })}
+            shifts.map((shift, index) => (
+              <ShiftButton
+                key={shift.id}
+                shift={shift}
+                summaryId={summaries[index] || undefined}
+                date={date}
+              />
+            ))}
         </div>
       </div>
     );
